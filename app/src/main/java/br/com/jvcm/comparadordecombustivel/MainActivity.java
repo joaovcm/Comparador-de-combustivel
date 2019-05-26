d1350b10bc249492b33c02c73dae44d83a213b04
Joao Victor Machado <joaovcm84@gmail.com>
2019-05-26 17:51:50 -0300

    Declarnado componente na MainActivity

--- app/src/main/java/br/com/jvcm/comparadordecombustivel/MainActivity.java
+++ app/src/main/java/br/com/jvcm/comparadordecombustivel/MainActivity.java
@@ -2,12 +2,25 @@ package br.com.jvcm.comparadordecombustivel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edtEthanol;
+    private EditText edtGasoline;
+    private Button btnCompare;
+    private View containerFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtEthanol = findViewById(R.id.edt_ethanol);
+        edtGasoline = findViewById(R.id.edt_gasoline);
+        btnCompare = findViewById(R.id.btn_compare);
+        containerFragment = findViewById(R.id.layout_container);
     }
 }
