0c3c54730865589393cc82ef8da613c2b043aadb
Joao Victor Machado <joaovcm84@gmail.com>
2019-05-26 18:03:45 -0300

    Implementando layout fragamento resultado e declarando classe fragmento resultado

---
+++ app/src/main/java/br/com/jvcm/comparadordecombustivel/FragmentResult.java
@@ -0,0 +1,24 @@
+package br.com.jvcm.comparadordecombustivel;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class FragmentResult extends Fragment {
+
+    private TextView tvResult;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_result, null);
+
+        tvResult = rootView.findViewById(R.id.tv_result);
+        return rootView;
+    }
+}
