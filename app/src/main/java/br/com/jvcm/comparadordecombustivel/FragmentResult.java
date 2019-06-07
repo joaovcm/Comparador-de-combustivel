2e2e4b9859dba328d45f5ccde97bfa4a33b97bd4
Joao Victor Machado <joaovcm84@gmail.com>
2019-06-07 12:41:30 -0300

    adicionando snackbar ao projeto e configurando o mesmo no FragmentResult

--- app/src/main/java/br/com/jvcm/comparadordecombustivel/FragmentResult.java
+++ app/src/main/java/br/com/jvcm/comparadordecombustivel/FragmentResult.java
@@ -1,8 +1,10 @@
 package br.com.jvcm.comparadordecombustivel;
 
+import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +14,8 @@ import android.widget.TextView;
 public class FragmentResult extends Fragment {
 
     private TextView tvResult;
+    private Snackbar snackbar;
+
 
     public static FragmentResult newInstance (String value){
         Bundle args =new Bundle();
@@ -33,5 +37,36 @@ public class FragmentResult extends Fragment {
             tvResult.setText(((MainActivity)getActivity()).getResult());
         }
         return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        if(tvResult == null){
+            showMessage("Sem valor inserido!");
+        }else {
+            showMessage("Valor calculado com sucesso!");
+        }
+    }
+
+    public void showMessage(String message) {
+
+        snackbar = Snackbar.make(getView(), "", Snackbar.LENGTH_LONG);
+        Snackbar.SnackbarLayout layout = (Snackbar.SnackbarLayout) snackbar.getView();
+        layout.setBackgroundResource(android.R.color.holo_red_dark);
+        LayoutInflater inflater = LayoutInflater.from(getView().getContext());
+        View view = inflater.inflate(R.layout.snackbar_custom_layout, null);
+        ((TextView) view.findViewById(R.id.tv_message)).setText(message);
+        layout.addView(view, 0);
+        snackbar.show();
+    }
+
+    @Override
+    public void onDestroyView() {
+        if(snackbar.isShown()){
+            snackbar.dismiss();
+        }
+        super.onDestroyView();
     }
 }
